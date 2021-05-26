6ee1ed2ca541d6b2013bcf990711ac381729082a
Sylvia D. Antonsdottir <sylvia@donchev.is>
2021-05-26 13:44:57 +0200

    Time and dates

---
+++ app.js
@@ -0,0 +1,43 @@
+function formatDate(timestamp) {
+  // calculate the date  return day and time
+  let date = new Date(timestamp)
+  let hours = date.getHours()
+  let minutes = date.getMinutes()
+  let days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    ' Friday',
+    'Saturday',
+  ]
+  let day = days[date.getDay()]
+  return `${day} ${hours}:${minutes}`
+}
+
+function displayTemperature(response) {
+  console.log(response.data)
+  let temperatureElement = document.querySelector('#temperature')
+  let cityElement = document.querySelector('#city')
+  let descriptionElement = document.querySelector('#description')
+  let humidityElement = document.querySelector('#humidity')
+  let windElement = document.querySelector('#wind')
+  let dateElement = document.querySelector('#date')
+
+  // Inner HTML stuff
+  descriptionElement.innerHTML = response.data.weather[0].description
+  cityElement.innerHTML = response.data.name
+  temperatureElement.innerHTML = Math.round(response.data.main.temp)
+  humidityElement.innerHTML = response.data.main.humidity
+  windElement.innerHTML = Math.round(response.data.wind.speed)
+  // get the current date and time
+  dateElement.innerHTML = formatDate(response.data.dt * 1000)
+}
+
+// To get current weather information
+let apiKey = '43700ee73704d4a7a92f7aa11e986149'
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=FRankfurt&appid=${apiKey}&units=metric`
+
+console.log(apiUrl)
+axios.get(apiUrl).then(displayTemperature)
