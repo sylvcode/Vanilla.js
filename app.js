72dff9c97d19625c1188bb5268163d5c90cd8b83
Sylvia D. Antonsdottir <sylvia@donchev.is>
2021-06-04 13:25:24 +0200

    error fixed

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   console.log(coordinates)
   let apiKey = '43700ee73704d4a7a92f7aa11e986149'
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lat}&appid=${apiKey}&units=metric`
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`
   axios.get(apiUrl).then(displayForecast)
 }
 
